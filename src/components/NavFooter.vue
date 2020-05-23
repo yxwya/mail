54916ec93370439ab994260d2a8c0cdb0770cbcb
yxwya <yxw159hh@163.com>
2020-05-23 23:33:06 +0800

    x项目目录结构

---
+++ src/components/NavFooter.vue
@@ -0,0 +1,9 @@
+<template>
+ <div>NavFooter</div>
+</template>
+
+<script>
+export default {
+  name: 'nav-footer',
+}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 // 配置
-module.exports = {
-    // 代理跨域
-    devServer:{
-        host:'localhost',
-        port:'8080',
-        proxy:{
+// module.exports = {
+//     // 代理跨域
+//     devServer:{
+//         host:'localhost',
+//         port:'8080',
+//         proxy:{
 
-        }
-    }
-}
+//         }
+//     }
+// }
